3acb3405b7beec0d9061b7e23c692f6ed2a50735
Christine Trant <christinetrant@gmail.com>
2020-05-22 21:22:28 +0100

    have shuffle function ready but not enabled, timer function starts on page refresh counts to 10

--- script.js
+++ script.js
@@ -12,9 +12,22 @@ let lastCard; // div of last card
 let currentCard; // div of current card
 
 // DOM Strings
-const timer = document.getElementById('timer');
+const time = document.getElementById('timer');
 const moves = document.getElementById('moves');
 const reset = document.getElementById('reset');
+
+// TIMER TO CHANGE!
+const timer = () => {
+  let i = 1;
+  let timer = setInterval(function() {
+    // console.log(i);
+    time.textContent = i;
+    i++;
+    if(i > 10) {
+       clearInterval(timer);
+    }
+  }, 1000);
+}
 
 // resets game
 const resetGame = () => { 
@@ -22,12 +35,22 @@ const resetGame = () => {
 	cards = [...card];
 	// counter = 0 as user only gets 2 cards to turn at a time
 	count = 0;
+	// start timer
+	timer();
 	// adds original classes to cards
 	cards.forEach(function (element, index){
 		element.classList.remove('card-rotate')
 		element.classList.remove('yellow');
 		element.classList.add('purple');	
 	})
+}
+// Shuffle Cards [Fisher Yates Shuffle]
+for(let i = cards.length - 1; i > 0; i--){
+  const j = Math.floor(Math.random() * i)
+  // swap elements array[i] and array[j]
+  const temp = cards[i]
+  cards[i] = cards[j]
+  cards[j] = temp
 }
 
 const displayCard = (element, index) => {
@@ -63,51 +86,49 @@ cards = cards.forEach((element, index) => {
   element.addEventListener("click", function() {  
 
   	if(count<1) {
-		console.log('first go', 'count:', count)
-		// play a card to turn
-		displayCard(element, index);
-	// display the elements while count is less than 2
-	} else if(count<=2) {
-		console.log('second & last go', 'count:', count)
-		// play a card to turn
-		displayCard(element, index);
-		// THINGS TO DO:
-		// 1. CHECK IF CARD IS THE SAME ONE CLICKED
-		if(lastCardIndex === currentCardIndex) {
-			// reset count
-			count = 0;
-			// call displayCard again 
+			console.log('first go', 'count:', count)
+			// play a card to turn
 			displayCard(element, index);
-		// 2. CHECK IF IT IS A MATCH
-		} else {
-			if(currentCardValue === lastCardValue) {
-				// 2B. IF A MATCH KEEP CARDS FLIPPED OVER
-				currentCard.classList.remove('blue');
-				currentCard.classList.add('yellow');
-				lastCard.classList.remove('blue');
-				lastCard.classList.add('yellow');
-				// filter out the cards that are not equal to the current match
-				cards = cards.filter(element => {
-					console.log(element)
-					return (element !== lastCard && element !== currentCard)
-				});
-				// 2C. CONTINUE GAME
-				clearBoard();				
-			// 3. IF NOT A MATCH RESET BOARD
+		// display the elements while count is less than 2
+		} else if(count<=2) {
+			console.log('second & last go', 'count:', count)
+			// play a card to turn
+			displayCard(element, index);
+			// THINGS TO DO:
+			// 1. CHECK IF CARD IS THE SAME ONE CLICKED
+			if(lastCardIndex === currentCardIndex) {
+				// reset count
+				count = 0;
+				// call displayCard again 
+				displayCard(element, index);
+			// 2. CHECK IF IT IS A MATCH
 			} else {
-				// clear board in 1 second as 2 cards will be showing
-				setTimeout(clearBoard, 1000);
+				if(currentCardValue === lastCardValue) {
+					// 2B. IF A MATCH KEEP CARDS FLIPPED OVER
+					currentCard.classList.remove('blue');
+					currentCard.classList.add('yellow');
+					lastCard.classList.remove('blue');
+					lastCard.classList.add('yellow');
+					// filter out the cards that are not equal to the current match
+					cards = cards.filter(element => {
+						return (element !== lastCard && element !== currentCard)
+					});
+					// 2C. CONTINUE GAME
+					clearBoard();				
+				// 3. IF NOT A MATCH RESET BOARD
+				} else {
+					// clear board in 1 second as 2 cards will be showing
+					setTimeout(clearBoard, 1000);
+				}
 			}
+		// count equals 2 - reset count	
+		} else if(count === 2) {
+			clearBoard();
 		}
-	} else if(count === 2) {
-		console.log('count equals 2 - reset count', count)
-		clearBoard();
-	}
-	// if there are no more cards in array - user has won
-	if(cards.length<1) {
-		console.log('You win! Game Over')
-	}
-	
+		// if there are no more cards in array - user has won
+		if(cards.length<1) {
+			console.log('You win! Game Over')
+		}	
 	})
 });
 
@@ -121,16 +142,3 @@ resetGame();
 // NEED TO RANDOMIZE CARDS!!!
 // at beginning of game show all cards then flip over - 5 seconds maybe?
 
-// TIMER TO CHANGE!
-// const timer = () => {
-//     var i = 1;
-//     var timer = setInterval(function() {
-//         console.log(i);
-//         i++;
-//         if(i > 10) {
-//             clearInterval(timer);
-//         }
-//     }, 1000);
-// }
-
-// timer();
