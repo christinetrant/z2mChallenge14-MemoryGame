3dcce5cef298e308d046068bfb75a20d0a0fb4cc
Christine Trant <christinetrant@gmail.com>
2020-05-27 19:49:33 +0100

    have object of card values and classes instead of html so it can be shuffled - have it adding shuffled value class to classlist

--- script.js
+++ script.js
@@ -21,21 +21,122 @@ const reset = document.getElementById('reset');
 const timeUl = document.getElementsByClassName('time-ul')[0]
 const movesUl = document.getElementsByClassName('moves-ul')[0]
 const resetUl = document.getElementsByClassName('reset-ul')[0]
+// variables for modals
+const playModal = document.getElementById("playModal");
+// Get the button that opens the modal
+const playBtn = document.getElementById("playBtn");
+const userTime = document.getElementById('userTime');
+const userMoves = document.getElementById('userMoves');
+// Win Game Modal!
+const winModal = document.getElementById("winModal");
+// Get the button that will play a new game
+const winBtn = document.getElementById("winBtn");
 
-// TIMER TO CHANGE!
-const timer = () => {
-  let i = 1;
-  let timer = setInterval(function() {
-    console.log(i);
-    time.textContent = i;
-    i++;
-    // stop timer if game is reset or game is won
-    if(playModal.style.display === 'block' || winModal.style.display === 'block') {
-       clearTimeout(timer);
-       i=0;
-    }
-  }, 1000); //roughly 1 second
+
+let testCards = [
+	{
+		class: 'tiger',
+		text: '🐯',
+	},
+	{
+		class: 'dolphin',
+		text: '🐬',
+	},
+	{
+		class: 'unicorn', 
+		text: '🦄',
+	},
+	{
+		class: 'ghost',
+		text: '👻',
+	},
+	{
+		class: 'alien',
+		text: '👽',
+	},
+	{
+		class: 'dinosaur',
+		text: '🦖',
+	},
+	{
+		class: 'unicorn',
+		text: '🦄',
+	},
+	{
+		class: 'tiger',
+		text: '🐯',
+	},
+	{
+		class: 'fish',
+		text: '🐠',
+	},
+	{
+		class: 'ghost',
+		text: '👻',
+	},
+	{
+		class: 'ladybug',
+		text: '🐞',
+	},
+	{
+		class: 'fish',
+		text: '🐠',
+	},
+	{
+		class: 'ladybug',
+		text: '🐞',
+	},
+	{
+		class: 'dolphin',
+		text: '🐬',
+	},
+	{
+		class: 'dinosaur',
+		text: '🦖',
+	},
+	{
+		class: 'alien',
+		text: '👽',
+	},
+]
+
+// Shuffle Cards
+const shuffleCards = (arr) => {
+	// Shuffle Cards [Fisher Yates Shuffle]
+	for(let i = arr.length - 1; i > 0; i--){
+	  const j = Math.floor(Math.random() * i);
+	  // swap elements array[i] and array[j]
+	  const temp = arr[i];
+	 	arr[i] = arr[j];
+	  arr[j] = temp;
+	} 
+	return arr;
 }
+
+let classTest = testCards.map(item => item.class)
+let textTest = testCards.map(item => item.text)
+
+for(let i=0; i<cards.length;i++) {
+	// for(let j=0; j<classTest.length;j++) {
+		classTest.map((item, i, arr) => {
+		cards[i].classList.add(item)
+	// }
+	})
+}
+// cards.map(obj => {
+// 	testCards.map(c => {
+// 	obj.classList.add(c.class)
+// 	obj.classList.add('purple')
+// 	obj.childNodes[1].childNodes[3].textContent = c.text
+// 	})
+// })
+// arr[i].childNodes[1].childNodes[3].textContent = temp.text
+		// arr[i].classList.add(temp.text)
+// cards.forEach(item => {
+// 	item.childNodes[1].childNodes[3].textContent = .text
+// 	item.classList.add(it.class)
+// 	item.classList.add('purple')
+// })
 
 // Play Game
 const playGame = () => {
@@ -50,7 +151,7 @@ const playGame = () => {
 	resetUl.classList.add('show');
 	
 	// start timer
-	timer();
+	// timer();
 
 	cards.forEach((element, index) => {
   element.addEventListener("click", function() {  
@@ -103,7 +204,7 @@ const playGame = () => {
 		}
 		// if there are no more cards in array - user has won
 		if(cards.length<1) {
-			console.log('You win! Game Over')
+			// console.log('You win! Game Over')
 			winModal.style.display = "block";
 			userTime.textContent = `Time Taken: ${time.textContent} seconds`;
 			userMoves.textContent = `Total Moves: ${userMove}`;
@@ -116,7 +217,6 @@ reset.addEventListener('click', playGame);
 
 // resets game
 const resetGame = () => { 
-
 	// call modal to begin game when ready
 	playModal.style.display = 'block';
 	// use spread operator to log all cards in array:
@@ -126,26 +226,17 @@ const resetGame = () => {
 	userMove = 0;
 	moves.textContent = 0;
 	time.textContent = 0;
+	// Shuffle Cards
+	// shuffleCards(cards);
 	// adds original classes to cards
-	cards.forEach(function (element, index){
+	cards.forEach((element, index) => {
 		element.classList.remove('card-rotate')
 		element.classList.remove('yellow');
 		element.classList.add('purple');	
 	})
 }
 
-
-
-
-// Shuffle Cards [Fisher Yates Shuffle]
-for(let i = cards.length - 1; i > 0; i--){
-  const j = Math.floor(Math.random() * i)
-  // swap elements array[i] and array[j]
-  const temp = cards[i]
-  cards[i] = cards[j]
-  cards[j] = temp
-}
-
+// Display a new card
 const displayCard = (element, index) => {
 	// add classes to rotate card
 	element.classList.add('card-rotate');
@@ -175,46 +266,36 @@ const clearBoard = () => {
 	})
 }
 
+// Timer function
+const timer = () => {
+  let i = 0;
+  let timer = setInterval(function() {
+    console.log(i);
+    time.textContent = i;
+    i++;
+    // stop timer if game is reset or game is won
+    if(playModal.style.display === 'block' || winModal.style.display === 'block') {
+       clearTimeout(timer);
+       i=0;
+    }
+  }, 1000); //roughly 1 second
+}
 
 
-
-
-
-
-// NEED TO RANDOMIZE CARDS!!!
-// at beginning of game show all cards then flip over - 5 seconds maybe?
-
-
-
-
-// Get the modal
-const playModal = document.getElementById("playModal");
-
-// Get the button that opens the modal
-const playBtn = document.getElementById("playBtn");
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-playBtn.addEventListener('click', function() {
-  // playModal.style.display = "block";
+// Modal functions
+// When the user clicks the button, play game and hide modal
+playBtn.addEventListener('click', () => {
   playGame();
   playModal.style.display = "none";
-
 })
 
-const userTime = document.getElementById('userTime');
-const userMoves = document.getElementById('userMoves');
-// Win Game Modal!
-const winModal = document.getElementById("winModal");
-// Get the button that will play a new game
-const winBtn = document.getElementById("winBtn");
-// Get the <span> element that closes the modal
-const winSpan = document.getElementsByClassName("close")[0];
 // When the user clicks the button play a new game
-winBtn.addEventListener('click', function() {
+winBtn.addEventListener('click', () => {
   playGame();
   winModal.style.display = "none";
   playModal.style.display = "none";
 })
+
+
+// NEED TO RANDOMIZE CARDS!!!
+// at beginning of game show all cards then flip over - 5 seconds maybe?
