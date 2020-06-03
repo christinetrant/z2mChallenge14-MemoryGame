c38c9fc07ddaeb375ddc5a5873510a593f131519
Christine Trant <christinetrant@gmail.com>
2020-06-03 17:25:08 +0100

    cleaned up code, added a few more comments and removed final code from restart folder into main folder

--- script.js
+++ script.js
@@ -1,95 +1,42 @@
-const card = document.getElementsByClassName('card');
+const card = document.querySelectorAll('.card');
 // use spread operator to log all cards in array:
-let cards = [...card];
-// counter = 0 as user only gets 2 cards to turn at a time
-let count, userMove, lastCardValue, currentCardValue, lastCardIndex, currentCardIndex, lastCard, currentCard;
+let cards, cardsFlipped;
 
+const playModal = document.getElementById('playModal');
+const playBtn = document.getElementById('playBtn');
+const winBtn = document.getElementById("winBtn");
 // DOM Strings
 const time = document.getElementById('timer');
 const moves = document.getElementById('moves');
 const reset = document.getElementById('reset');
-
 // ul to hide and display
 const scoreBoard = document.getElementsByClassName('score-ul')[0]
 // update text content of moves and times
 const userTime = document.getElementById('userTime');
 const userMoves = document.getElementById('userMoves');
-// Win Game Modal!
-const winModal = document.getElementById('winModal');
-// Get the button that will play a new game
-const winBtn = document.getElementById("winBtn");
-// variables for modals
-const playModal = document.getElementById('playModal');
-// Get the button that opens the modal
-const playBtn = document.getElementById("playBtn");
 
+let firstCard, secondCard;
+let hascardbeenflipped = false;
+
+// The cards we want to play in the game
 let testCards = [
-	{
-		cardClass: 'tiger',
-		cardText: '🐯',
-	},
-	{
-		cardClass: 'dolphin',
-		cardText: '🐬',
-	},
-	{
-		cardClass: 'unicorn', 
-		cardText: '🦄',
-	},
-	{
-		cardClass: 'ghost',
-		cardText: '👻',
-	},
-	{
-		cardClass: 'alien',
-		cardText: '👽',
-	},
-	{
-		cardClass: 'dinosaur',
-		cardText: '🦖',
-	},
-	{
-		cardClass: 'unicorn',
-		cardText: '🦄',
-	},
-	{
-		cardClass: 'tiger',
-		cardText: '🐯',
-	},
-	{
-		cardClass: 'fish',
-		cardText: '🐠',
-	},
-	{
-		cardClass: 'ghost',
-		cardText: '👻',
-	},
-	{
-		cardClass: 'ladybug',
-		cardText: '🐞',
-	},
-	{
-		cardClass: 'fish',
-		cardText: '🐠',
-	},
-	{
-		cardClass: 'ladybug',
-		cardText: '🐞',
-	},
-	{
-		cardClass: 'dolphin',
-		cardText: '🐬',
-	},
-	{
-		cardClass: 'dinosaur',
-		cardText: '🦖',
-	},
-	{
-		cardClass: 'alien',
-		cardText: '👽',
-	},
+	{	cardClass: 'tiger', cardText: '🐯', },
+	{ cardClass: 'dolphin', cardText: '🐬', },
+	{ cardClass: 'unicorn', cardText: '🦄', },
+	{ cardClass: 'ghost', cardText: '👻', },
+	{ cardClass: 'alien', cardText: '👽', },
+	{ cardClass: 'dinosaur', cardText: '🦖', },
+	{ cardClass: 'unicorn', cardText: '🦄', },
+	{ cardClass: 'tiger', cardText: '🐯', },
+	{ cardClass: 'fish', cardText: '🐠', },
+	{ cardClass: 'ghost', cardText: '👻', },
+	{ cardClass: 'ladybug', cardText: '🐞', },
+	{ cardClass: 'fish', cardText: '🐠', },
+	{ cardClass: 'ladybug', cardText: '🐞', },
+	{ cardClass: 'dolphin', cardText: '🐬', },
+	{ cardClass: 'dinosaur', cardText: '🦖', },
+	{ cardClass: 'alien', cardText: '👽', },
 ]
-
 // Shuffle Cards
 const shuffleCards = () => {
 	// Shuffle Cards [Fisher Yates Shuffle]
@@ -123,147 +70,90 @@ const shuffleCards = () => {
 	textTest.map((item, i) => cards[i].childNodes[1].childNodes[3].textContent = item)
 }
 
-// Play Game
-const playGame = (element, index) => {
-	// Call init function to reset game stats and cards;
-	resetGame();
-
-	scoreBoard.classList.add('show');
-	
-	// start timer
-	timer();
-
-	// MAKE EVENT LISTENER A FUNCTION THEN WE CAN DISABLE FOR MATCHED CARDS
-	cards.forEach((element, index) => {
-  element.addEventListener("click", function() {  
-
-  	if(count<1) {
-			console.log('first go', 'count:', count)
-			// play a card to turn
-			displayCard(element, index);
-		// display the elements while count is less than 2
-		
-		} else if(count<=2) {
-			console.log('second & last go', 'count:', count)
-			// play a card to turn
-			displayCard(element, index);
-			// THINGS TO DO:
-			// 1. CHECK IF CARD IS THE SAME ONE CLICKED
-			if(lastCardIndex === currentCardIndex) {
-				// reset count
-				count = 0;
-				// call displayCard again 
-				displayCard(element, index);
-			// 2. CHECK IF IT IS A MATCH
-			} else {
-				if(currentCardValue === lastCardValue) {
-					// 2B. IF A MATCH KEEP CARDS FLIPPED OVER
-					currentCard.classList.remove('blue');
-					currentCard.classList.add('yellow');
-					currentCard.classList.add('disable');
-					lastCard.classList.remove('blue');
-					lastCard.classList.add('yellow');
-					lastCard.classList.add('disable');
-					console.log('Match!')
-					
-					// filter out the cards that are not equal to the current match
-					cards = cards.filter(element => {
-						return (element !== lastCard && element !== currentCard)
-					});
-					// 2C. CONTINUE GAME
-					clearBoard();	
-					userMove++;
-					moves.textContent = userMove;			
-				// 3. IF NOT A MATCH RESET BOARD
-				} else {
-					// clear board in 1 second as 2 cards will be showing
-					setTimeout(clearBoard, 1000);
-					userMove++;
-					moves.textContent = userMove;
-				}
-			}
-		// count equals 2 - reset count	
-		} else if(count === 2) {
-			clearBoard();
-			userMove++;
-			moves.textContent = userMove;
-		}
-		// if there are no more cards in array - user has won
-		if(cards.length<1) {
-			// console.log('You win! Game Over')
-			winModal.style.display = "block";
-			userTime.textContent = `Time Taken: ${time.textContent} seconds`;
-			userMoves.textContent = `Total Moves: ${userMove}`;
-		}	
-	})
-});
-}
-// Event Listener for Play Game
-// reset.addEventListener('click', playGame);
-
-// resets game
-const resetGame = () => { 
-	// counter = 0 as user only gets 2 cards to turn at a time
-	count = 0;
-	userMove = 0;
-	moves.textContent = 0;
-	time.textContent = 0;
-
-	// to store value of card so we can match it
-	lastCardValue = ''; // e.g. unicorn
-	currentCardValue = ''; // e.g. tiger
-	lastCardIndex = ''; // index of last card
-	currentCardIndex = ''; // index of current card
-	lastCard = ''; // div of last card
-	currentCard = ''; // div of current card
-
-	playModal.style.display = "none";
-	winModal.style.display = "none";
-
-	// use spread operator to log all cards in array:
-	cards = [...card];
-
-	// adds original classes to cards
-	cards.forEach((element, index) => {
-		element.classList.remove('card-rotate')
-		element.classList.remove('yellow');
-		element.classList.add('purple');	
-	})	
-
-	// Shuffle Cards
-	shuffleCards();
-	reset.addEventListener('click', reloadPage)
-}
-const reloadPage = () => location.reload();
-
-// Display a new card
-const displayCard = (element, index) => {
-	// add classes to rotate card
-	element.classList.add('card-rotate');
-	// change color of overturned cards
-	element.classList.remove('purple');
-	element.classList.add('blue');
-	// increment value of count
-	count++;
-	// save last card before overwriting current card
-	lastCardValue = currentCardValue;
-	lastCard = currentCard;
-	lastCardIndex = currentCardIndex
-	// card value becomes the current element
-	currentCardValue = element.classList[1];
-	currentCard = element;
-	currentCardIndex = index;
-}
-
-// function turns cards back over face down
-const clearBoard = () => { 
-	cards.forEach((element, index) => {
+// Clears the board
+const clearBoard = () => {
+	cards.forEach((element) => {
 		element.classList.remove('card-rotate');
 		element.classList.remove('blue');
 		element.classList.add('purple');
-		// reset count
-		count = 0;
+		element.classList.remove('disable');
 	})
+}
+
+// Checks for a match when two cards are flipped
+const checkMatch = () => {
+	cards.forEach((element) => {
+		element.classList.add('disable');
+	})
+	let firstType = firstCard.classList[1];
+	let secondType = secondCard.classList[1];
+	if(firstType === secondType) {
+		// 2B. IF A MATCH KEEP CARDS FLIPPED OVER
+		firstCard.classList.remove('blue');
+		firstCard.classList.add('yellow');
+		firstCard.classList.add('disable');
+		secondCard.classList.remove('blue');
+		secondCard.classList.add('yellow');
+		// filter out the cards that are not equal to the current match
+		cards = cards.filter(element => {
+			return (element !== firstCard && element !== secondCard)
+		});
+		clearBoard();
+	} else {
+		// clear board in 1 second as 2 cards will be showing
+		setTimeout(clearBoard, 1000);
+	}
+	hascardbeenflipped = false;
+}
+
+const checkWin = () => {
+	// if there are no more cards in array - user has won
+	if(cards.length<1) {
+		winModal.style.display = "block";
+		userTime.textContent = `Time Taken: ${time.textContent} seconds`;
+		userMoves.textContent = `Total Moves: ${userMove}`;
+	}
+}
+
+function displayCard() {
+	// add classes to rotate card
+	this.classList.add('card-rotate');
+	// change color of overturned cards
+	this.classList.remove('purple');
+	this.classList.add('blue');
+	// disable card from being clicked
+	this.classList.add('disable');
+	// check to see if a card has been turned or not and assign accordingly
+	if(!hascardbeenflipped) {
+		firstCard = this;
+		hascardbeenflipped = true;
+	} else {
+		secondCard = this;
+		checkMatch();
+		checkWin();
+		userMove++;
+		moves.textContent = userMove;	
+	}
+}
+
+const startGame = () => {
+	playModal.style.display = 'none';
+	winModal.style.display = 'none';
+	// display score boards
+	scoreBoard.classList.add('show');
+	// start timer
+	timer();
+	// user moves starts at zero and display time and moves
+	userMove = 0;
+	moves.textContent = 0;
+	time.textContent = 0;
+	// no cards are flipped to begin with
+	hascardbeenflipped = false;
+	cards = [...card];
+	shuffleCards();
+	clearBoard();
+	// for each card add event listener
+	card.forEach(item => item.addEventListener('click', displayCard))
 }
 
 // Timer function
@@ -273,21 +163,20 @@ const timer = () => {
     time.textContent = i;
     i++;
     // stop timer if game is reset or game is won
-    // if(playModal.style.display === 'block' || winModal.style.display === 'block') {
-    if(winModal.style.display === 'block') {
+    if(playModal.style.display === 'block' || winModal.style.display === 'block') {
       clearTimeout(timer);
-      // i=0;
     }
   }, 1000); //roughly 1 second
 }
 
+// Event Listeners
+// When user clicks button in either play or win modal, play game
+playBtn.addEventListener('click', startGame);
+winBtn.addEventListener('click', startGame);
+// if reset game pressed - bring up playmodal
+reset.addEventListener('click', () => {
+	scoreBoard.classList.add('hide');
+	playModal.style.display = 'block'
+})
 
-// Modal functions
-// When the user clicks the button, play game and hide modal
-playBtn.addEventListener('click', playGame);
 
-// When the user clicks the button play a new game
-winBtn.addEventListener('click', reloadPage)
-// winBtn.addEventListener('click', resetGame)
-
-// at beginning of game show all cards then flip over - 5 seconds maybe?
